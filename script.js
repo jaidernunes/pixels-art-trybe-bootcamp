3adebf02536d33220340b69708df456bcab8eff2
jaidernunes <jaidernf@gmail.com>
2022-08-02 18:52:02 -0300

    6 :ok

--- script.js
+++ script.js
@@ -4,5 +4,19 @@ function paintBox() {
   color[1].style.backgroundColor = 'red';
   color[2].style.backgroundColor = 'green';
   color[3].style.backgroundColor = 'blue';
+  color[0].addEventListener('click', clickColor);
+  color[1].addEventListener('click', clickColor);
+  color[2].addEventListener('click', clickColor);
+  color[3].addEventListener('click', clickColor);
+
 }
 paintBox();
+
+function clickColor () {
+    const color = document.getElementsByClassName('color');
+    for (let i = 0; i < color.lenght; i += 0) {
+        color[i].className = 'color';
+    }
+    clickColor.target.className = 'color selected';
+    let activeColor = clickColor.target.style.backgroundColor;
+}
