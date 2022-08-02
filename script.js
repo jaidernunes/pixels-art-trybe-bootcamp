a3761da12b0ad143046a4bada6bc02f3e39c277a
jaidernunes <jaidernf@gmail.com>
2022-08-02 15:37:10 -0300

    Req. 1, 2 and 3 :ok:

---
+++ script.js
@@ -0,0 +1,8 @@
+function paintBox() {
+  const color = document.getElementsByClassName('color');
+  color[0].style.backgroundColor = 'black';
+  color[1].style.backgroundColor = 'red';
+  color[2].style.backgroundColor = 'green';
+  color[3].style.backgroundColor = 'blue';
+}
+paintBox();
