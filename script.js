7f8b0d2f5f66278a400ea58dd7abed7919d383fd
jaidernunes <jaidernf@gmail.com>
2022-08-02 19:56:30 -0300

    reqs. 7 and 8 :ok: -- project APPROVED

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
+/* eslint-disable no-param-reassign */
 function paintBox() {
   const color = document.getElementsByClassName('color');
   color[0].style.backgroundColor = 'black';
   color[1].style.backgroundColor = 'red';
   color[2].style.backgroundColor = 'green';
   color[3].style.backgroundColor = 'blue';
-  color[0].addEventListener('click', clickColor);
-  color[1].addEventListener('click', clickColor);
-  color[2].addEventListener('click', clickColor);
-  color[3].addEventListener('click', clickColor);
-
+  color[0].addEventListener('click', clickPalette);
+  color[1].addEventListener('click', clickPalette);
+  color[2].addEventListener('click', clickPalette);
+  color[3].addEventListener('click', clickPalette);
 }
 paintBox();
 
-function clickColor () {
+function clickPalette (event) {
     const color = document.getElementsByClassName('color');
-    for (let i = 0; i < color.lenght; i += 0) {
-        color[i].className = 'color';
-    }
-    clickColor.target.className = 'color selected';
-    let activeColor = clickColor.target.style.backgroundColor;
+    color[0].className = 'color';
+    color[1].className = 'color';
+    color[2].className = 'color';
+    color[3].className = 'color';
+    // const color = document.getElementsByClassName('color');
+    // for (let i = 0; i < color.lenght; i += 0) {
+    //     color[i].className = 'color';
+    //     console.log(color[i])
+    // } NAO SEI PQ NAO FUNCIONOU @_@
+    event.target.className = 'color selected';
+}
+
+function applyColor (event) {
+    let selectedColor = document.getElementsByClassName('selected')[0];
+    event.target.style.backgroundColor = selectedColor.style.backgroundColor;
+}
+
+let pixel = document.getElementsByClassName('pixel');
+for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].addEventListener('click', applyColor);
 }
